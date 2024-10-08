087cdb1ca374380b2e8bfc233af4e89cbf0e6e80
syedmehedi34 <syedmeehdi34@gmail.com>
2024-10-08 09:58:13 +0600

    discuss section design

---
+++ js/script.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        btnColor: "#797DFC",
+        bgColor: "#12132D",
+        primary: "#12132D",
+        primary60: "#12132D99",
+        primary80: "#12132DCC",
+        btnActive: "#10B981",
+        btnInActive: "#FF3434",
+      },
+      fontFamily: {
+        Mulish: "Mulish",
+        Inter: "Inter",
+      },
+      backgroundImage: {
+        bg1: "url('./images/leaf01.png')",
+        gradient_bg:
+          "linear-gradient(to right, #bcbef8, #8f8ece, #6460a5, #3a347d, #0a0c56)",
+      },
+    },
+  },
+};
